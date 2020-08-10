ef572cfc22b9bb9bc0c9d2fe2f94931d0ede1f6e
Seongwon-developer <ysw991106@gmail.com>
2020-08-10 19:31:11 +0900

    simple project ~ing

--- simpleProject/ToDoList/app.js
+++ simpleProject/ToDoList/app.js
@@ -1,30 +1,31 @@
-var express = require('express');
-var routes = require('./routes');
-var todo = require('./routes/todo');
-var http = require('http');
-var path = require('path');
+// 모듈 가져오기
+var express = require('express')
+  , routes = require('./routes')
+  , todo = require('./routes/todo')
+  , http = require('http')
+  , path = require('path');
 
-var app = express();
-var port = 3000;
+var app = express();	// 어플리케이션 생성
+var port = 3000;		// 어플리케이션 포트
 
 // 어플리케이션 설정
 app.configure(function(){
-  app.set('port', port);                  // 웹 서버 포트
-  app.set('view', __dirname + '/views');  // 템플릿
-  app.set('view engine', 'ejs');          // 템플릿 엔진
-  app.use(express.favicon());             // 파비콘
-  app.use(express.logger('dev'));         // 로그 기록
-  app.use(express.bodyParser());          // 요청 본문 파싱
-  app.use(express.methodOverride());      // 구식 브라우저 메소드 지원
-  app.use(app.router);                    // 라우팅
-
+  app.set('port', port);					// 웹 서버 포트
+  app.set('views', __dirname + '/views');	// 템플릿
+  app.set('view engine', 'ejs');			// 템플릿 엔진
+  app.use(express.favicon());				// 파비콘
+  app.use(express.logger('dev'));			// 로그 기록
+  app.use(express.bodyParser());			// 요청 본문 파싱
+  app.use(express.methodOverride());		// 구식 브라우저 메소드 지원
+  app.use(app.router);						// 라우팅
+	
   // 정적 리소스 처리
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){    // 개발 버전
-  app.use(express.errorHandler());          // 에러 메세지
+app.configure('development', function(){	// 개발 버전
+  app.use(express.errorHandler());			// 에러 메세지
 });
 
 // 라우팅
@@ -36,7 +37,5 @@ app.post('/del', todo.del);
 
 // 서버 실행
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Server Running');
+  console.log("Express server listening on port " + app.get('port'));
 });
-
-// module.exports = app;
--- simpleProject/ToDoList/routes/index.js
+++ simpleProject/ToDoList/routes/index.js
@@ -1,5 +1,3 @@
 exports.index = function(req, res){
-  res.render('index', {
-    title : '간단한 ToDoList 예제 실습'
-  });
+  res.render('index', { title: '간단한 ToDo 리스트 예제 실습' });
 };
