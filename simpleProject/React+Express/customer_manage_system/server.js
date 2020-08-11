6e68fa4b7dfc3fc35995cb33b7cfe70b118b54a3
Seongwon-developer <ysw991106@gmail.com>
2020-08-11 22:24:32 +0900

    ~ing

---
+++ simpleProject/React+Express/customer_manage_system/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id' : 1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '양성원',
+            'birthday' : '991106',
+            'gender' : '남성',
+            'job' : '대학생'
+        },
+        {
+            'id' : 2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '강윤주',
+            'birthday' : '000110',
+            'gender' : '여성',
+            'job' : '대학생'
+        }
+    ]);
+});
+
+app.listen(port, () =>{
+    console.log('Server Running');
+});
